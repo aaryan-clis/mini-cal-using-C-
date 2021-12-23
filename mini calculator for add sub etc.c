cfbe9e4d86b63610818a7fee83a3f55b66dc4596
Aaryan <72549723+aaryan-clis@users.noreply.github.com>
2021-12-23 23:45:26 +0530

    Initial

---
+++ mini calculator for add sub etc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int a , b   ;
+    printf("We will take two values and print their additon , subraction , multiplication and division \n");
+    printf("Enter first number");
+    scanf("%d",&a);
+    printf("Enter second number ");
+    scanf("%d",&b);
+    printf("The addition of a+b is :- %d\n ",a+b);
+    printf("The subraction of a-b is :- %d\n ",a-b);
+    printf("The multiplication of a-b is :- %d\n ",a*b);
+    printf("The division of a-b is :- %d\n ",a/b);
+
+    return 0;
+}
